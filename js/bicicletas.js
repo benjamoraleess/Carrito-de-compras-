aaa508f346c56d79b29c1a8406cb8c0c95d81fc5
Benja <you@proa.com>
2024-11-06 11:55:06 -0300

    V1

---
+++ js/bicicletas.js
@@ -0,0 +1,33 @@
+/* Esta lista podría ser reemplazada por la respuesta de un backend */
+const bicicletas = [
+  {
+    id:1,
+    nombre: "Iphone 14",
+    precio: 111111,
+  },
+  {
+    id:2,
+    nombre: "Iphone 14 pro",
+    precio: 222222,
+  },
+  {
+    id:3,
+    nombre: "Iphone XR",
+    precio: 333333,
+  },
+  {
+    id:4,
+    nombre: "Iphone 11",
+    precio: 444444,
+  },
+  {
+    id:5,
+    nombre: "Iphone 13",
+    precio: 555555,
+  },
+  {
+    id:6,
+    nombre: "Iphone 7",
+    precio: 666666,
+  }
+]
